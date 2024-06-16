311e98db1169362fe19b4f6d99bce5da03d274ba
atsuki-t <atsuki-t@weseek.co.jp>
2024-06-16 02:48:09 +0000

    show run time

--- exec-main.sh
+++ exec-main.sh
@@ -1,1 +1,13 @@
-g++ main.cpp -o main && ./main < main.in
+#!/bin/bash
+
+# コンパイル
+g++ main.cpp -o main
+
+start=$(date +%s.%N)
+# 実行
+./main < main.in
+end=$(date +%s.%N)
+
+# 実行時間を計算して表示する
+runtime=$(echo "scale=2; ($end - $start) * 1000 / 1" | bc)
+echo -e "\n実行時間: $runtime ms"
