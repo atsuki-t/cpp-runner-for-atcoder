3816ba4774e31bec5c2a9636438bf605175f6d68
atsuki-t <atsuki-t@weseek.co.jp>
2024-06-22 11:30:28 +0000

    set timeout

--- exec-main.sh
+++ exec-main.sh
@@ -5,9 +5,15 @@ g++ main.cpp -std=gnu++20 -o main
 
 start=$(date +%s.%N)
 # 実行
-./main < main.in
-end=$(date +%s.%N)
+timeout 10s ./main < main.in
 
-# 実行時間を計算して表示する
-runtime=$(echo "scale=2; ($end - $start) * 1000 / 1" | bc)
-echo -e "\n実行時間: $runtime ms"
+# 直前のコマンドがタイムアウトしたらメッセージを表示する
+if [ $? -eq 124 ]; then
+  echo "実行時間が10秒を超えたので強制的に中断しました"
+else
+  end=$(date +%s.%N)
+
+  # 実行時間を計算して表示する
+  runtime=$(echo "scale=2; ($end - $start) * 1000 / 1" | bc)
+  echo -e "\n実行時間: $runtime ms"
+fi
